241feae7673db4f3fb35f8514a998be85a42d741
Michael Andrew <michaelandrew228@gmail.com>
2019-12-23 19:57:30 +0700

    baca rfid 2 arduino

---
+++ file_arduino/bacadataserial/bacadataserial.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial port2(3, 2);
+String informasi, informasi2;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+port2.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available() > 0){
+    
+    informasi = Serial.readStringUntil("\n");
+    Serial.print("INFO :" +informasi );
+  
+  }
+
+  port2.listen();
+
+  while(port2.available() > 0){
+    
+    informasi2 = port2.readStringUntil("\n");
+    Serial.print("INFO :" +informasi2 );
+  
+  }
+}
+
+
+
